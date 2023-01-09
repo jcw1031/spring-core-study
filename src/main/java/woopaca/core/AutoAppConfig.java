2fd361cef0b4b70cd19f47ad0b3fd8bf5e50507e
woopaca <jcw001031@gmail.com>
2023-01-09 15:07:20 +0900

    빈 중복 등록

--- src/main/java/woopaca/core/AutoAppConfig.java
+++ src/main/java/woopaca/core/AutoAppConfig.java
@@ -1,17 +1,21 @@
 package woopaca.core;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import woopaca.core.member.MemberRepository;
+import woopaca.core.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(
-        basePackages = "woopaca.core.member",
         excludeFilters = @ComponentScan.Filter(
                 type = FilterType.ANNOTATION, classes = Configuration.class
         )
 )
 public class AutoAppConfig {
-
-
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
