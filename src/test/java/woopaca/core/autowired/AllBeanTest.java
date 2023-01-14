ee60b683e39c52f30eef3b36379bb35877945b97
woopaca <jcw001031@gmail.com>
2023-01-14 18:56:37 +0900

    Map, List를 사용한 조회된 모든 빈 주입

---
+++ src/test/java/woopaca/core/autowired/AllBeanTest.java
@@ -0,0 +1,52 @@
+package woopaca.core.autowired;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import woopaca.core.AutoAppConfig;
+import woopaca.core.discount.DiscountPolicy;
+import woopaca.core.member.Grade;
+import woopaca.core.member.Member;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AllBeanTest {
+
+    @Test
+    void test() {
+        ApplicationContext ac;
+        ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+        Member member = new Member(1L, "wooapca", Grade.VIP);
+        int discountPrice = discountService.discout(member, 20000, "fixDiscountPolicy");
+
+        assertThat(discountPrice).isEqualTo(1000);
+
+        int rateDiscountPrice = discountService.discout(member, 20000, "rateDiscountPolicy");
+
+        assertThat(rateDiscountPrice).isEqualTo(2000);
+    }
+
+    static class DiscountService {
+
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policyList;
+
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policyList) {
+            this.policyMap = policyMap;
+            this.policyList = policyList;
+
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policyList = " + policyList);
+        }
+
+        public int discout(Member member, int price, String discountCode) {
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+            return discountPolicy.discount(member, price);
+        }
+    }
+}
