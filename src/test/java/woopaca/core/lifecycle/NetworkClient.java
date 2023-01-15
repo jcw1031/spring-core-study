45076c65d6ec888aa626ed5e25eab100c805deb5
woopaca <jcw001031@gmail.com>
2023-01-15 12:58:24 +0900

    @Bean initMethod와 destroyMethod

--- src/test/java/woopaca/core/lifecycle/NetworkClient.java
+++ src/test/java/woopaca/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package woopaca.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient /*implements InitializingBean, DisposableBean*/ {
 
     private String url;
 
@@ -29,7 +26,7 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
+    /*@Override
     public void afterPropertiesSet() throws Exception {
         connect();
         call("초기화 연결 메시지");
@@ -37,6 +34,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
 
     @Override
     public void destroy() throws Exception {
+        disconnect();
+    }*/
+
+    public void init() {
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void close() {
         disconnect();
     }
 }
