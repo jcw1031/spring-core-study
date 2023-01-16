e3919f9066d4f0d64adebfba15e26636ed9155bc
woopaca <jcw001031@gmail.com>
2023-01-16 14:45:27 +0900

    ObjectProvider 사용

--- src/test/java/woopaca/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/woopaca/core/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,7 @@
 package woopaca.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -22,20 +23,21 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
 
-        private final PrototypeBean prototypeBean;
+        private final ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
-        //        @Autowired
-        ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        // @Autowired
+        ClientBean(ObjectProvider<PrototypeBean> prototypeBeanProvider) {
+            this.prototypeBeanProvider = prototypeBeanProvider;
         }
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
